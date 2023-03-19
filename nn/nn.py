fe1abd394dc3ed6c3312fde4aa0292bfd86b25ab
jonathanhb <jonathan3141@gmail.com>
2023-03-18 23:37:27 -0700

    commit 1

--- nn/nn.py
+++ nn/nn.py
@@ -10,7 +10,7 @@ class NeuralNetwork:
     Parameters:
         nn_arch: List[Dict[str, float]]
             A list of dictionaries describing the layers of the neural network.
-            e.g. [{'input_dim': 64, 'output_dim': 32, 'activation': 'relu'}, {'input_dim': 32, 'output_dim': 8, 'activation:': 'sigmoid'}]
+            e.g. [{'input_dim': 64, 'output_dim': 32, 'activation': 'relu'}, {'input_dim': 32, 'output_dim': 8, 'activation': 'sigmoid'}]
             will generate a two-layer deep network with an input dimension of 64, a 32 dimension hidden layer, and an 8 dimensional output.
         lr: float
             Learning rate (alpha).
@@ -28,15 +28,16 @@ class NeuralNetwork:
             (see nn_arch above)
     """
 
+    #note that Union was originally called with parentheses and threw a syntax error
     def __init__(
         self,
-        nn_arch: List[Dict[str, Union(int, str)]],
+        nn_arch: List[Dict[str, Union[int, str]]],
         lr: float,
         seed: int,
         batch_size: int,
         epochs: int,
         loss_function: str
-    ):
+    ):  #<-- winter quarter sad face close parenthesis
 
         # Save architecture
         self.arch = nn_arch
@@ -106,7 +107,26 @@ class NeuralNetwork:
             Z_curr: ArrayLike
                 Current layer linear transformed matrix.
         """
-        pass
+        # dictionary of activation functions without leading underscores
+        activation_dict = {"sigmoid":self._sigmoid}
+
+        #print("matmul shapes:")
+        #print(W_curr.shape)
+        #print(A_prev.shape)
+        #print(b_curr.shape)
+
+        # print(W_curr)
+        # print(A_prev)
+        # print(np.matmul(W_curr, np.transpose(A_prev)))
+        # print(np.tile(b_curr, A_prev.shape[0]))
+
+        #linear transform of the inputs
+        Z_curr = np.transpose(np.matmul(W_curr, np.transpose(A_prev)) + np.tile(b_curr, A_prev.shape[0]))
+
+        #feed proto-outputs through activation function
+        A_curr = np.array([activation_dict[activation](i) for i in Z_curr])
+
+        return (A_curr, Z_curr)
 
     def forward(self, X: ArrayLike) -> Tuple[ArrayLike, Dict[str, ArrayLike]]:
         """
@@ -122,7 +142,31 @@ class NeuralNetwork:
             cache: Dict[str, ArrayLike]:
                 Dictionary storing Z and A matrices from `_single_forward` for use in backprop.
         """
-        pass
+
+        #fix list formatting; adding matrices will not work otherwise
+        if len(X.shape) == 1:
+            X = np.reshape(X, (1, X.shape[0]))
+
+        #The input to the next layer, starting with the function's argument.
+        #at each step of the loop below, this is set to the single_layer_pass output
+        last_output = X
+
+        #linear and activation matrices for each layer
+        cache = {}
+
+        for idx in range(int(len(self._param_dict)/2)):
+            layer = idx + 1
+            #feed the output of the previous layer through the current layer
+            print(self.arch[idx])
+            single_layer_pass = self._single_forward(self._param_dict["W"+str(layer)], self._param_dict["b"+str(layer)], last_output, self.arch[idx]['activation'])
+            #store the output of the current layer to be fed into the next layer
+            last_output = single_layer_pass[0]
+            #add entries to the dictionary
+            cache["A" + str(layer)] = single_layer_pass[0]
+            cache["Z" + str(layer)] = single_layer_pass[1]
+
+        return (last_output, cache)
+
 
     def _single_backprop(
         self,
@@ -158,9 +202,30 @@ class NeuralNetwork:
             db_curr: ArrayLike
                 Partial derivative of loss function with respect to current layer bias matrix.
         """
+
+        activation_dict = {"sigmoid":self._sigmoid_backprop}
+
+        ###dL/dA_prev = dL/dA_curr * dA_curr/dA_prev = (dL/dA_curr * dA_curr/dZ_curr) * dZ_curr/dA_prev
+
+        #derivative of the loss function L with respect to the transformed inputs Z
+        #dimensions of len(z)
+        dLdZ = activation_dict[activation_curr](dA_curr, Z_curr)
+        print(dLdZ.shape)
+        #^ this equals the derivative of L with respect to the constants b since dZ/db = 1
+
+        #derivative of the loss function with respect to the inputs A_in
+        #dimensions of len(A_in)
+        dLdA_in = np.matmul(dLdZ, np.transpose(W_curr))
+        print(dLdA_in.shape)
+
+
+
+        return (dLdA_in,  dLdZ)
+
         pass
 
     def backprop(self, y: ArrayLike, y_hat: ArrayLike, cache: Dict[str, ArrayLike]):
+
         """
         This method is responsible for the backprop of the whole fully connected neural network.
 
@@ -177,6 +242,22 @@ class NeuralNetwork:
             grad_dict: Dict[str, ArrayLike]
                 Dictionary containing the gradient information from this pass of backprop.
         """
+
+        nlayers = len(self.arch)
+
+        grad_dict = {}
+
+        #derivative of the loss with respect to the activation output
+        dLdA_out = self._binary_cross_entropy_backprop(y, y_hat)
+
+        for idx in range(nlayers):
+            invidx = nlayers-idx-1
+
+
+
+            dLdA_out =
+
+
         pass
 
     def _update_params(self, grad_dict: Dict[str, ArrayLike]):
@@ -217,7 +298,31 @@ class NeuralNetwork:
             per_epoch_loss_val: List[float]
                 List of per epoch loss for validation set.
         """
-        pass
+
+        per_obs_loss_train = []
+        per_obs_loss_val = []
+
+        for i, xtr in enumerate(X_train):
+            print("aaaaaaa")
+            print(xtr)
+            print(i)
+            #if i == 1:
+            #    import sys
+            #    sys.exit(0)
+
+            #predict from the current observation
+            fpass = self.forward(xtr)
+            #calculate the training loss
+            per_obs_loss_train.append(self._binary_cross_entropy(y_train[i], fpass[0]))
+
+            #calculate the gradient and update the weights
+            grad = self.backprop(y_train[i], fpass[0], fpass[1])
+            self._update_params(grad)
+
+            #compute validation loss
+            per_obs_loss_val.append(self._binary_cross_entropy(y_val, self.predict(X_val)))
+
+
 
     def predict(self, X: ArrayLike) -> ArrayLike:
         """
@@ -231,7 +336,9 @@ class NeuralNetwork:
             y_hat: ArrayLike
                 Prediction from the model.
         """
-        pass
+
+        #return the output of one forward pass through the network
+        return self.forward(X)[0]
 
     def _sigmoid(self, Z: ArrayLike) -> ArrayLike:
         """
@@ -245,7 +352,9 @@ class NeuralNetwork:
             nl_transform: ArrayLike
                 Activation function output.
         """
-        pass
+        #using a logistic sigmoid function
+        return [1/(1+np.exp(-i)) for i in Z]
+
 
     def _sigmoid_backprop(self, dA: ArrayLike, Z: ArrayLike):
         """
@@ -261,7 +370,8 @@ class NeuralNetwork:
             dZ: ArrayLike
                 Partial derivative of current layer Z matrix.
         """
-        pass
+
+        return [dA[i]*np.exp(-Z[i])/(1+np.exp(-Z[i]))**-2 for i in range(len(dA))]
 
     def _relu(self, Z: ArrayLike) -> ArrayLike:
         """
@@ -307,7 +417,12 @@ class NeuralNetwork:
             loss: float
                 Average loss over mini-batch.
         """
-        pass
+        n = y.shape[0]
+
+        # compute the binary cross entropy loss function. First term is added for true observations, second term is added for false ones.
+        #from hw7
+        bce_loss = -sum([y_hat[x] * np.log(y_hat[x]) + (1 - y_hat[x]) * (np.log(1 - y_hat[x])) for x in range(n)]) / n
+        return bce_loss
 
     def _binary_cross_entropy_backprop(self, y: ArrayLike, y_hat: ArrayLike) -> ArrayLike:
         """
@@ -321,9 +436,12 @@ class NeuralNetwork:
 
         Returns:
             dA: ArrayLike
-                partial derivative of loss with respect to A matrix.
+                partial derivative of loss with respect to y_hat.
         """
-        pass
+        # dividing by y.shape[0] should make the learning rate choice less sensitive to batch size
+        # np.matmul(y_hat - y, X)  # / y.shape[0]
+        #from hw7
+        return (y_hat - y)/(y_hat*(1 - y_hat))
 
     def _mean_squared_error(self, y: ArrayLike, y_hat: ArrayLike) -> float:
         """
@@ -356,3 +474,88 @@ class NeuralNetwork:
                 partial derivative of loss with respect to A matrix.
         """
         pass
+
+
+#---------------------------------------------------------------------------
+#--------------------------------  testing  --------------------------------
+#---------------------------------------------------------------------------
+
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+
+#----------------------------------------------
+# generate data
+# generate a pair of 2d gaussians and label them and see if the method can distinguish the points
+
+#distributions for classes 0 and 1
+n0 = 500
+mean0 = [0,0]
+cov0 = [[1,0],[0,1]]
+n1 = 600
+mean1 = [2,3]
+cov1 = [[1,1],[1,.5]]
+#number of input features
+nf = len(mean1)
+
+#generate data and labels
+#ignore the error about positive semidefiniteness
+d0 = np.random.multivariate_normal(mean0, cov0, n0)
+d1 = np.random.multivariate_normal(mean1, cov1, n1)
+
+#combine the data points
+data = np.concatenate((d0,d1))
+
+#label each gaussian and combine the labels
+labels = np.reshape(np.concatenate((np.zeros(n0),np.ones(n1))), (n0+n1,1))
+
+#combine data points and their labels
+data_labels = np.concatenate((data,labels), axis=1)
+
+#shuffle combined data and labels before use
+np.random.shuffle(data_labels)
+
+#plot the data
+plot = False
+if plot:
+    plt.scatter(data_labels[:,0], data_labels[:,1], c=data_labels[:,2])
+    plt.show()
+
+#prepare data and labels for testing
+n_train = 700
+x_train = data_labels[0:n_train,0:nf]
+y_train = np.reshape(data_labels[0:n_train,nf], (n_train, 1))
+x_val = data_labels[n_train:,0:nf]
+y_val = np.reshape(data_labels[n_train:,nf], (n0+n1-n_train, 1))
+
+print(x_train.shape)
+print(y_train.shape)
+print(x_val.shape)
+print(y_val.shape)
+
+#----------------------------------------------
+#initialize neural network
+test_nn = NeuralNetwork(
+    [{'input_dim': nf, 'output_dim': 2, 'activation': 'sigmoid'}, {'input_dim': 2, 'output_dim': 1, 'activation': 'sigmoid'}],
+    lr=.1,
+    seed=0,
+    batch_size=10,
+    epochs=10,
+    loss_function="sigmoid"
+    )
+
+test_nn.fit(x_train, y_train, x_val, y_val)
+
+
+
+#print(test_nn.predict(x_train[0]))
+#print(y_train[0])
+#print("bce")
+#print(test_nn._binary_cross_entropy(y_train[0], test_nn.predict(x_train[0])))
+
+#trimmings
+# print(f"z curr shape: {Z_curr.shape}")
+# print(f"w curr shape: {W_curr.shape}")
+# print(f"a prev shape: {A_prev.shape}")
+# print(f"b curr shape: {b_curr.shape}")
+# print(np.matmul(W_curr, A_prev).shape)
